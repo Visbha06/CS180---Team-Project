885796f1945193edfa31d5b221d7edfaeb1c02ce
braydenhipp <hippb@purdue.edu>
2024-11-01 18:28:36 -0400

    Finishes two methods, fixes some errors, and adds java doc

--- MessagesDatabase.java
+++ MessagesDatabase.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
 import java.io.*;
 
+/**
+ * MessagesDatabase.java
+ * Creates a database that stores and accesses messages from the user
+ *
+ * @author Brayden Hipp
+ * @version November 31st 2024
+ */
+
 public class MessagesDatabase extends Thread implements Database {
     //Fields
-    private ArrayList<String> userData;
+    private ArrayList<String> userData; //messages from users [username]: [message]
     private final static Object gateKeeper = new Object();
 
     //constructor
@@ -45,7 +53,7 @@ public class MessagesDatabase extends Thread implements Database {
     }
 
     // TODO: Change implementation -- should return ArrayList<String> foundMessages
-    public boolean findMessages(String username) {
+    public ArrayList<String> findMessages(String username) {
         synchronized (gateKeeper) {
             //stores all the messages by a certain username arrayList
             ArrayList<String> foundMessages = new ArrayList<>();
@@ -54,34 +62,65 @@ public class MessagesDatabase extends Thread implements Database {
             for (int i = 0; i < userData.size(); i++) {
                 if (userData.get(i).contains(username)) {
                     foundMessages.add(userData.get(i));
-                    return true;
+                    return foundMessages;
                 }
             }
+            return foundMessages;
         }
-        return false;
+
     }
 
     public boolean deleteMessage(String username, String message) {
         synchronized (gateKeeper) {
+            int originalSize = userData.size();
             //goes through userData and removes the message from the txt file
             for (int i = 0; i < userData.size(); i++) {
                 if (userData.get(i).contains(username) && userData.get(i).contains(message)) {
                     userData.remove(i);
-                    return true;
+                    i++;
+                }
+
+            }
+            if (originalSize > userData.size()) {
+                return true; //if any messages were deleted
+            } else {
+                return false; // if no messages were deleted
+            }
+
+        }
+
+    }
+
+    //goes through the arrayList userData and returns
+    //a new arrayList that has the messages from all users excluding their own
+    public ArrayList<String> messagesAllUsers(String username) {
+        synchronized (gateKeeper) {
+            ArrayList<String> allUserMessages = new ArrayList<>();
+
+            for (int i = 0; i < userData.size(); i++) {
+                if (userData.get(i).contains(username)) {
+                    userData.remove(i);
+                    i++;
                 }
             }
+            allUserMessages.addAll(userData); //sets allUserMessages to the modified userData
+            return allUserMessages;
         }
-        return false;
-    }
-
-    public boolean messagesAllUsers(String username) {
-        return true; //WIP
-
-    }
-
-    public boolean messagesOnlyFriends(String username) {
-        return true; //WIP
     }
 
 
+    public ArrayList<String> messagesOnlyFriends(String userName, String[] friends) {
+        synchronized (gateKeeper) {
+            ArrayList<String> friendsOnly = new ArrayList<>();
+
+            for (int i = 0; i < userData.size(); i++) {
+                for (int j = 0; j < friends.length; j++) {
+                    if (userData.get(i).contains(friends[j])) {
+                        friendsOnly.add(userData.get(i));
+                    }
+                }
+            }
+            return friendsOnly;
+        }
+    }
 }
