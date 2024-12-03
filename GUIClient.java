fc6df4ee552e63eeb5e914dc2764cd20047746fb
Visbha06 <bhat79@purdue.edu>
2024-12-02 19:03:47 -0500

    Added buttons and fields to GUIClient, updated some features.

--- GUIClient.java
+++ GUIClient.java
@@ -16,20 +16,25 @@ public class GUIClient extends JComponent implements Runnable {
 
     private static final String SERVER_ADDRESS = "localhost";
     private static final int PORT = 1234;
+    private final String TITLE = "Chat";
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
 
+    private JTextField textField;
+    private JButton enterBtn;
+    private JTextField searchField;
+    private JButton msgToggle;
+
     ActionListener actionListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            switch (e.getSource()) {
-                case null -> {
+            if (e.getSource() == textField) {
 
-                }
-                default -> {
+            } else if (e.getSource() == enterBtn) {
 
-                }
+            } else if (e.getSource() == searchField) {
+
             }
         }
     };
@@ -54,7 +59,45 @@ public class GUIClient extends JComponent implements Runnable {
 
     @Override
     public void run() {
-        JFrame frame = new JFrame("Chat"); // To be changed
+        int reply = JOptionPane.NO_OPTION;
+        do {
+            String username = JOptionPane.showInputDialog(null, "Please enter your username", TITLE,
+                    JOptionPane.QUESTION_MESSAGE);
+            String password = JOptionPane.showInputDialog(null, "Please enter your password", TITLE,
+                    JOptionPane.INFORMATION_MESSAGE);
+
+            out.write(username + ":" + password);
+            out.println();
+            out.flush();
+
+            String response = "";
+            try {
+                response = in.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (!response.equals("LOGIN ERROR")) {
+                createGUI();
+            } else {
+                JOptionPane.showMessageDialog(null, "Login failed", TITLE, JOptionPane.ERROR_MESSAGE);
+                reply = JOptionPane.showConfirmDialog(null, "Do you want to try again?", TITLE,
+                        JOptionPane.YES_NO_OPTION);
+            }
+        } while (reply == JOptionPane.YES_OPTION);
+    }
+
+    private void createGUI() {
+        JFrame frame = new JFrame(TITLE);
+
+        textField = new JTextField();
+        textField.addActionListener(actionListener);
+        enterBtn = new JButton();
+        enterBtn.addActionListener(actionListener);
+        searchField = new JTextField();
+        searchField.addActionListener(actionListener);
+        msgToggle = new JButton();
+        msgToggle.addActionListener(actionListener);
 
         frame.setSize(600, 400);
         frame.setLocationRelativeTo(null);
