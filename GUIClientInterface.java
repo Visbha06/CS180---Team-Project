3521cd8decfc6f43bfd7f8ce000de1c4250b23b6
ryleeholler <hollerr@purdue.edu>
2024-12-03 23:17:30 -0500

    I created the interface for the GUIClient class and the javadoc for it.

---
+++ GUIClientInterface.java
@@ -0,0 +1,12 @@
+/**
+ * GUIClientInterface.java
+ * Creates an interface for the GUI Client Class of the platform
+ *
+ * @author Rylee Holler -- Section L25
+ * @version 03 December 2024
+ */
+
+public interface GUIClientInterface {
+
+    void run();
+}
